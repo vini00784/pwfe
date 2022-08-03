42122b5752e733455bbcfdc9a05bfadde2c1d323
Vinicius Santos <vo70346@gmail.com>
2022-08-03 10:40:52 -0300

    Programa que altera cor

--- aula002/app.js
+++ aula002/app.js
@@ -1,8 +1,21 @@
 'use strict'
 
 function aplicarCor() {
-    const cor = document.getElementById('cor').value
-    document.body.style.backgroundColor = cor
+    const nomeCor = document.getElementById('cor').value.toUpperCase()
+    
+    if(nomeCor == 'VERMELHO') {
+        document.documentElement.style.setProperty('--bg-color', `#FF0000`)
+    } else if(nomeCor == 'AZUL') {
+        document.documentElement.style.setProperty('--bg-color', `#0000FF`)
+    } else if(nomeCor == 'VERDE') {
+        document.documentElement.style.setProperty('--bg-color', `#00FF00`)
+    } else if(nomeCor == 'AMARELO') {
+        document.documentElement.style.setProperty('--bg-color', `#FFFF00`)
+    } else {
+        alert('Opção Inválida!')
+    }
+
+    // document.documentElement.style.setProperty('--bg-color', $,{nomeCor})
 }
 
 document.getElementById('aplicar').addEventListener('click', aplicarCor)
